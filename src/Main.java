08005a7d1a6f358862938e27146640b8337d2ff3
Yulia <kreydunovay@gmail.com>
2022-04-25 14:53:45 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialAccount = 100;
+        int replenishmentAmount = 2000;
+        int amountRubles = 100;
+
+        int bonus;
+        int grandTotal;
+
+        if (replenishmentAmount > 1000) {
+            bonus = (replenishmentAmount / amountRubles);
+            grandTotal = initialAccount + replenishmentAmount + bonus;
+        } else {
+            bonus = 0;
+            grandTotal = initialAccount + replenishmentAmount;
+        }
+
+        System.out.println("Итоговый счет: " + grandTotal );
+        System.out.println("Бонусов: " + bonus);
+
+
+    }
+}
